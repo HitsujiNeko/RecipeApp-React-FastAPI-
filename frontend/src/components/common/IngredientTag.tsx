66264f9653edc1ed3fde5f4a0500d9503693acec
HitsujiNeko <a21tr029@st.osaka-cu.ac.jp>
2025-10-23 11:21:48 +0900

    タグをtypeに基づいてスタイリングするように修正

--- frontend/src/components/common/IngredientTag.tsx
+++ frontend/src/components/common/IngredientTag.tsx
@@ -1,13 +1,33 @@
-import { IngredientModel } from "../../types/models"
+import { IngredientModel } from "../../types/models";
 
 interface IngredientTagProps {
-  ingredient: IngredientModel; 
+  ingredient: IngredientModel;
 }
 
-export default function IngredientTag(ing : IngredientTagProps) {
+function getTypeStyle(type: string) {
+  switch (type) {
+    case "肉":
+      return "text-red-700 border-red-700";
+    case "魚":
+      return "text-blue-700 border-blue-700";
+    case "野菜":
+      return "text-green-700 border-green-700";
+    case "炭水化物":
+      return "text-yellow-700 border-yellow-700";
+    default:
+      return "text-gray-700 border-gray-300";
+  }
+}
+
+export default function IngredientTag({ ingredient }: IngredientTagProps) {
   return (
-    <span className="bg-orange-100 border border-orange-700 text-sm font-medium rounded-2xl px-2 py-1">
-      {ing.ingredient.name}
+    <span
+      className={`
+        border text-sm font-medium rounded-2xl px-2 py-1
+        ${getTypeStyle(ingredient.type)}
+        `}
+    >
+      {ingredient.name}
     </span>
-  )
+  );
 }
